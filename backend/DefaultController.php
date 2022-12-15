d9f2624c4c277a6a774f14d7f469e1880c46541b
moncefSaiki <moncefsaiki@hotmail.fr>
2022-12-15 09:21:37 +0100

    Rebasing app
    try to fix 3
    
    try 4
    
    try tof
    
    try to fix .env
    
    try try
    
    try to to
    
    comment
    
    try to deploy
    
    retry
    
    reetry
    
    add secret
    
    try add port
    
    change path
    
    try
    
    test
    
    try to test
    
    change path
    
    retry
    
    re
    
    tt
    
    adding composer install
    
    fix
    
    refix
    
    ddd
    
    jsp
    
    bridge
    
    dd
    
    push image to registrt
    
    wait and see
    
    reetry
    
    retry
    
    ht
    
    fff
    
    v
    
    vv
    
    re-re
    
    en
    
    eee
    
    try to add container
    
    rm sudo
    
    replace apk
    
    add
    
    jsp
    
    aa
    
    re
    
    aaaaaa
    
    test
    
    jsp
    
    jsp
    
    last
    
    laaast
    
    def
    
    working
    
    work
    
    retttt
    
    run
    
    ran
    
    fin
    
    ff
    
    eeedefe
    
    zz
    
    hhh
    
    c i
    
    reinstall
    
    debug composer
    
    debug
    
    retry
    
    aaaaa
    
    compser2
    
    composer.lock
    
    cp files
    
    ret
    
    jsp
    
    show
    
    erq
    
    e
    
    whynt
    
    try production
    
    add htacces
    
    clean ci
    
    retry
    
    re
    
    jsp
    
    e
    
    symfon
    
    script
    
    noooo
    
    retry
    
    passcaaa
    
    rebuild with mysql
    
    test back database
    
    ignore.env
    
    ;env
    
    push

--- backend/DefaultController.php
+++ backend/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Back;
+namespace App\Controller\Front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +11,6 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'default_index')]
     public function index(): Response
     {
-        return new Response('on est sur la prod');
+        return $this->render('front/default/index.html.twig');
     }
 }
