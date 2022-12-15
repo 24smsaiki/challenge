0dc71dc7bb3aff8ddf0827bda5757a7e17f8f405
moncefSaiki <moncefsaiki@hotmail.fr>
2022-12-15 09:56:44 +0100

    try try

---
+++ backend/src/Entity/User.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TimestampableTrait;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?array $roles = [];
+
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[Length(min: 8)]
+    private ?string $plainPassword = null;
+
+    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Statistic::class)]
+    private Collection $pokemons;
+
+    public function __construct()
+    {
+        $this->pokemons = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string|null $plainPassword
+     * @return User
+     */
+    public function setPlainPassword(?string $plainPassword): User
+    {
+        $this->plainPassword = $plainPassword;
+
+        if ($plainPassword) {
+            $this->setUpdatedAt(new \DateTime());
+        }
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, Statistic>
+     */
+    public function getPokemons(): Collection
+    {
+        return $this->pokemons;
+    }
+
+    public function addPokemon(Statistic $pokemon): self
+    {
+        if (!$this->pokemons->contains($pokemon)) {
+            $this->pokemons->add($pokemon);
+            $pokemon->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removePokemon(Statistic $pokemon): self
+    {
+        if ($this->pokemons->removeElement($pokemon)) {
+            // set the owning side to null (unless already changed)
+            if ($pokemon->getOwner() === $this) {
+                $pokemon->setOwner(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ frontend/src/components/security/RegisterForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <section>
+      <form @submit.prevent="createUser">
+        <div>
+          <label for="email">Email :</label>
+          <input type="email" id="email" v-model="userData.email">
+        </div>
+        <div>
+          <label for="password">Password: </label>
+          <input type="password" id="password" v-model="userData.password">
+        </div>
+        
+        <button>Create User</button>
+      </form>
+    </section>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        userData: {
+          email:  '',
+          password:   ''
+        }
+      }
+    },
+    methods: {
+        createUser() {
+          axios
+            .post('https://localhost/user/new', this.userData)
+            .then((response) => console.log(response))
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  html,body{margin:0;padding:0}
+  section{height:100vh;display:grid;justify-items:center;padding-top:40px}
+  div{margin:24px auto}
+  label{font-weight:bolder;display:block;margin-bottom:4px}
+  </style>
