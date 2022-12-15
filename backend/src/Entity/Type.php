19109ccf2c4b65d25dd10fb9e966d7d2dd6bd741
moncefSaiki <moncefsaiki@hotmail.fr>
2022-12-15 08:45:34 +0100

    inital commit

---
+++ backend/src/Entity/Type.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\SortablePosition;
+
+#[ORM\Entity(repositoryClass: TypeRepository::class)]
+class Type
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: Pokemon::class, mappedBy: 'type')]
+    private Collection $pokemon;
+
+    #[SortablePosition]
+    #[ORM\Column(nullable: true)]
+    private ?int $position;
+
+    public function __construct()
+    {
+        $this->pokemon = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Pokemon>
+     */
+    public function getPokemon(): Collection
+    {
+        return $this->pokemon;
+    }
+
+    public function addPokemon(Pokemon $pokemon): self
+    {
+        if (!$this->pokemon->contains($pokemon)) {
+            $this->pokemon->add($pokemon);
+            $pokemon->addType($this);
+        }
+
+        return $this;
+    }
+
+    public function removePokemon(Pokemon $pokemon): self
+    {
+        if ($this->pokemon->removeElement($pokemon)) {
+            $pokemon->removeType($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int|null $position
+     * @return Type
+     */
+    public function setPosition(?int $position): Type
+    {
+        $this->position = $position;
+        return $this;
+    }
+}
