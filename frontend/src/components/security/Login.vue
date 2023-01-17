a320ae6b556474dbdab0809b42640c8746b7c41e
moncef-24s <m.saiki@24s.com>
2023-01-17 13:00:07 +0100

    initialization

---
+++ frontend/src/components/security/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <section>
+      <form @submit.prevent="createUser">
+        <div>
+          <label for="email">Email :</label>
+          <input type="email" id="email" v-model="userData.email">
+        </div>
+        <div>
+          <label for="password">Password: </label>
+          <input type="password" id="password" v-model="userData.password">
+        </div>
+        
+        <button>Create User</button>
+      </form>
+    </section>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        userData: {
+          email:  '',
+          password:   ''
+        }
+      }
+    },
+    methods: {
+        createUser() {
+          axios
+            .post('https://api-challenge.moon-factory.fr/auth', this.userData)
+            .then((response) => console.log(response))
+            .then( data => {
+                localStorage.setItem('token',response.data.token)
+            })
+        }
+    }
+  }
+  </script>
+  
+  <style>
+  html,body{margin:0;padding:0}
+  section{height:100vh;display:grid;justify-items:center;padding-top:40px}
+  div{margin:24px auto}
+  label{font-weight:bolder;display:block;margin-bottom:4px}
+  </style>
---
+++ frontend/src/components/security/UsersList.vue
@@ -0,0 +1,86 @@
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api-challenge.moon-factory.fr/user/all')
+      .then((response) => {
+        this.users = response.data
+      })
+  }
+}
+</script>
+
+
+
+<template>
+    <div class="container">
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Email</th>
+                <th>Roles</th>
+                <th>Action</th>
+            </tr>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.roles }}</td>
+                <td>A venir</td>
+            </tr>
+      
+        </table>
+
+    </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+  table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+
+}
+</style>
+
+
