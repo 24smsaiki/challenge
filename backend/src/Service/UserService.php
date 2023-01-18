8ce82288e3bc4b8e312d70757f0b541cde00104b
moncef-24s <m.saiki@24s.com>
2023-01-18 16:52:04 +0100

    user service

---
+++ backend/src/Service/UserService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserService
+{
+    private $tokenStorage;
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    
+    public function getCurrent()
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if (!$token) {
+            return null;
+        }
+    
+        $user = $token->getUser();
+        
+    
+        return $user;
+    }
+}
+    
