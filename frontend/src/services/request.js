593fbfacbf9c5cc7ed6167b18e7e1c49fe2e9fee
YacineSoussi <Yacine1.soussi@gmail.com>
2023-01-18 09:39:53 +0100

    Create request.js

---
+++ frontend/src/services/request.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import LocalStorage from "./localStorage";
+
+const request = axios.create({
+    baseURL: "http://localhost",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+});
+
+request.interceptors.request.use(
+    (config) => {
+        const token = LocalStorage.get("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }
+);
+
+export default request;
+
+export const make = async(method, url, data) => {
+    const config = {
+        method,
+        url,
+        data,
+    };
+
+    try {
+        const response = await request(config);
+        return response.data;
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
+
+
