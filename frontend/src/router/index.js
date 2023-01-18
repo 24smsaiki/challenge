77c420d7be1d7d07855bc855fdff93e848d7e07f
YacineSoussi <Yacine1.soussi@gmail.com>
2023-01-18 11:57:49 +0100

    implement template

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from "../views/Home.vue";
+import CategoryPage from "../views/CategoryPage.vue";
+import ProductPage from "../views/ProductPage.vue";
+import Checkout from "../views/Checkout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: "/", name: "Home", component: Home },
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../components/security/UsersList.vue')
+      path: "/category/:category",
+      name: "CategoryPage",
+      component: CategoryPage,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/product/:product",
+      name: "ProductPage",
+      component: ProductPage,
+    },
+    {
+      path: "/checkout",
+      name: "Checkout",
+      component: Checkout,
+    },
+    {
+      path: '/users',
+      name: 'users',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../components/security/UsersList.vue')
+    },
   ]
 })
 
