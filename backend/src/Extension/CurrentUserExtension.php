f44889e558edbc9af49198ace6d9b6baa9d16ba1
alilou-dev <raidali95@yahoo.com>
2023-01-25 00:00:09 +0100

    extentions for orders inprogress

--- backend/src/Extension/CurrentUserExtension.php
+++ backend/src/Extension/CurrentUserExtension.php
@@ -2,34 +2,35 @@
 
 namespace App\Extension;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+
 use App\Entity\Order;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use ApiPlatform\Metadata\Operation;
+use Symfony\Component\Security\Core\Security;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 
 /**
  * This extension makes sure normal users can only access their own Orders
  */
 final class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
-    private $authorizationChecker;
-    
-    public function __construct(private AuthorizationCheckerInterface $authChecker)
+    private $securityChecker;
+    public function __construct(Security $security)
     {
-        $this->authorizationChecker = $authChecker;
+        $this->securityChecker = $security;
     }
 
     /**
      * @param QueryBuilder $queryBuilder
      * @param QueryNameGeneratorInterface $queryNameGenerator
      * @param string $resourceClass
-     * @param string|null $operationName
+     * @param Operation $operation
+     * @param array $context
      */
-    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []) : void
     {
-        dd("hello");
         $this->addWhere($queryBuilder, $resourceClass);
     }
 
@@ -38,12 +39,11 @@ final class CurrentUserExtension implements QueryCollectionExtensionInterface, Q
      * @param QueryNameGeneratorInterface $queryNameGenerator
      * @param string $resourceClass
      * @param array $identifiers
-     * @param string|null $operationName
+     * @param Operation $operation
      * @param array $context
      */
-    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, Operation $operation = null, array $context = []): void
     {
-        dd("hello");
         $this->addWhere($queryBuilder, $resourceClass);
     }
 
@@ -54,16 +54,13 @@ final class CurrentUserExtension implements QueryCollectionExtensionInterface, Q
      */
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
     {
-        if (Order::class !== $resourceClass
-            || $this->authorizationChecker->isGranted('ROLE_ADMIN')
-            || null === $user = $this->authorizationChecker->getUser()
-        ) {
+        if (Order::class !== $resourceClass || $this->securityChecker ->isGranted('ROLE_ADMIN') || null === $user = $this->securityChecker->getUser()) {
             return;
         }
-
         $rootAlias = $queryBuilder->getRootAliases()[0];
-        $queryBuilder->join(sprintf('%s.users', $rootAlias), 'users')
-            ->andWhere('users.id = :user')
-            ->setParameter('user', $this->authorizationChecker->getUser()->getId());
+        dd($queryBuilder->andWhere(sprintf('%s.user = :current_user', $rootAlias)));
+
+        $queryBuilder->setParameter('current_user', $user->getId());           
+
     }
 }
