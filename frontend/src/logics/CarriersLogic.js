837d2fc1ad4b7b66f9329ca643d701a3ded627db
Pereira Júlio <julioocesarr@outlook.fr>
2023-02-09 19:30:28 +0100

    Spacing + indentation

--- frontend/src/logics/CarriersLogic.js
+++ frontend/src/logics/CarriersLogic.js
@@ -1,19 +1,19 @@
 import CarriersRepository from "../repositories/CarriersRepository";
 
 export default class CarriersLogic {
-    static async getCarriers() {
-        return await CarriersRepository.getCarriers();
-    };
+  static async getCarriers() {
+    return await CarriersRepository.getCarriers();
+  }
 
-    static async getCarrier(id) {
-        return await CarriersRepository.getCarrier(id);
-    };
+  static async getCarrier(id) {
+    return await CarriersRepository.getCarrier(id);
+  }
 
-    static async createCarrier(body) {
-        return await CarriersRepository.createCarrier(body);
-    };
+  static async createCarrier(body) {
+    return await CarriersRepository.createCarrier(body);
+  }
 
-    static async updateCarrier(id, body) {
-        return await CarriersRepository.updateCarrier(id, body);
-    };
+  static async updateCarrier(id, body) {
+    return await CarriersRepository.updateCarrier(id, body);
+  }
 }
