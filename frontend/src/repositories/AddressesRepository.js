ac100b58e348e018645f343dcb2ab9ac85d39634
Pereira Júlio <julioocesarr@outlook.fr>
2023-02-09 19:17:46 +0100

    Spaces + indentation

--- frontend/src/repositories/AddressesRepository.js
+++ frontend/src/repositories/AddressesRepository.js
@@ -1,20 +1,19 @@
-import * as Request from '../services/request';
+import * as Request from "../services/request";
 
 export default class AddressesRepository {
-    static async getAddresses() {
-        return await Request.make('get', '/addresses');
-    };
+  static async getAddresses() {
+    return await Request.make("get", "/addresses");
+  }
 
-    static async getAddress(id) {
-        return await Request.make('get', `/addresses/${id}`);
-    };
+  static async getAddress(id) {
+    return await Request.make("get", `/addresses/${id}`);
+  }
 
-    static async createAddress(body) {
-        return await Request.make('post', '/addresses', {...body});
-    };
+  static async createAddress(body) {
+    return await Request.make("post", "/addresses", { ...body });
+  }
 
-    static async updateAddress(id, body) {
-        return await Request.make('put', `/addresses/${id}`, {...body});
-    };
+  static async updateAddress(id, body) {
+    return await Request.make("put", `/addresses/${id}`, { ...body });
+  }
 }
-
