6cf67dda0eceb571403b348e100fd30cd056bb41
alilou-dev <raidali95@yahoo.com>
2023-02-10 17:09:13 +0100

    fix logic for seller informations

--- frontend/src/logics/Account/seller/ProfileLogic.js
+++ frontend/src/logics/Account/seller/ProfileLogic.js
@@ -1,7 +1,11 @@
 import ProfileRepository from "../../../repositories/Account/seller/ProfileRepository";
 
 export default class ProfileLogic {
-  static async getSeller() {
-    return await ProfileRepository.getSeller();
+  static async getSellerInformation() {
+    return await ProfileRepository.getSellerInformation();
+  }
+
+  static async getShopInformation() {
+    return await ProfileRepository.getShopInformation();
   }
 }
