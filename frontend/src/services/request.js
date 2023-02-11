b709bf7eab0db277c9a08d85179d7c216f3b30f7
Pereira Júlio <julioocesarr@outlook.fr>
2023-02-11 17:20:14 +0100

    Remove previous behavior of request

--- frontend/src/services/request.js
+++ frontend/src/services/request.js
@@ -1,6 +1,5 @@
 import LocalStorage from "./localStorage";
 import axios from "axios";
-import router from "../router/Router";
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -14,11 +13,6 @@ request.interceptors.request.use((config) => {
   const token = LocalStorage.get("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-  } else {
-    localStorage.removeItem("app-token");
-    localStorage.removeItem("app-user");
-    router.push({ name: "Login" });
-    window.location.reload();
   }
 
   return config;
