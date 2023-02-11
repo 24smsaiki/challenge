bc3657d87e8d167668b377fc713dd3a78191e28c
alilou-dev <raidali95@yahoo.com>
2023-02-11 16:28:23 +0100

    add send email for request account seller (declined or accpeted)

--- backend/api/src/Controller/ManageRequestAccountSellerController.php
+++ backend/api/src/Controller/ManageRequestAccountSellerController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\Seller;
 
+use App\Service\MailService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -15,26 +16,28 @@ class ManageRequestAccountSellerController extends AbstractController
 {
     public function __construct(
         private RequestStack $requestStack,
-        private ManagerRegistry $managerRegistry        
+        private ManagerRegistry $managerRegistry,
+        private MailService $mailService        
     ) {}
 
   
     public function __invoke(int $id)
     {
         $body = json_decode($this->requestStack->getCurrentRequest()->getContent(),true);
-        
+        $getSeller = $this->managerRegistry->getRepository(Seller::class)->findOneById($id);
+        $email = $getSeller->getShopEmailContact();
+        $emailContent = "";
+        $emailSubject = "";
         switch ($body['answer'])
         {
-            // TODO : communiquer plus tard avec le front le 'accepted_request' et 'declined_request'
             case 'accepted_request':
                 $em = $this->managerRegistry->getManager();
-                $getSeller = $em->getRepository(Seller::class)->findOneById($id);
-                if ($getSeller->getIsRequested() === true &&$getSeller->getIsActif() === false) {
-                    
+                if ($getSeller->getIsRequested() === true && $getSeller->getIsActif() === false) {
+
                     $getSeller->setIsActif(true);
                     $user = new User();
-    
-                    $user->setEmail($getSeller->getShopEmailContact());
+
+                    $user->setEmail($email);
                     $user->setPassword($getSeller->getPassword());
                     $user->setRoles(["ROLE_SELLER"]);
                     $user->setIsActif(true);
@@ -48,7 +51,9 @@ class ManageRequestAccountSellerController extends AbstractController
                    
                     $em->persist($getSeller);
                     $em->flush();
-                    
+                    $emailContent = "<h3>votre compte vendeur a été validé vous pouvez désormais vous connecté avec l'adresse suivante : ".$email."<h3>";
+                    $emailSubject = "Votre compte est pret"; 
+
                     return new JsonResponse(['message' => 'seller request accepted', 'status' => 'success'], 201);
                 } else {
                     return new JsonResponse(['message' => 'something went wrong retry', 'status' => 'success'], 201);
@@ -61,7 +66,14 @@ class ManageRequestAccountSellerController extends AbstractController
                 $getSeller->setIsDeclined(true);
                 $em->persist($getSeller);
                 $em->flush();
+
+                $emailContent = "<h3>votre compte vendeur a été refusé vous pouvez contacter nos services pour plus d'inforamtions<h3>";
+                $emailSubject = "Compte refusé"; 
+
                 return new JsonResponse(['message' => 'seller request not Accepted', 'status' => 'success'], 201);
+
+
+            $this->mailService->send($email,$emailSubject,$emailContent);    
         }
                 
     }
