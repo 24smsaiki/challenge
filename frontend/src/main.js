9064849f3bcf51b72f8b6c02a33aece7d9b68966
Pereira Júlio <julioocesarr@outlook.fr>
2023-02-13 19:02:08 +0100

    Refactoring

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,6 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import router from "./router/Router";
 
-
 library.add(fas);
 
 const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
