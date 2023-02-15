b1377da53d0732a79afd6c6395c6bcd448eaeb5a
Pereira Júlio <julioocesarr@outlook.fr>
2023-02-15 13:52:11 +0100

    Fix style

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,40 +1,41 @@
 import "./index.css";
 
+import {
+  faCheck,
+  faCirclePlus,
+  faEye,
+  faMessage,
+  faPaperPlane,
+  faPenToSquare,
+  faRightFromBracket,
+  faRobot,
+  faTrash,
+  faUsers,
+  faXmark,
+  fas,
+} from "@fortawesome/free-solid-svg-icons";
+
 import App from "./App.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { 
-    fas, 
-    faCheck,
-	faCirclePlus,
-	faEye,
-	faMessage,
-	faPaperPlane,
-	faPenToSquare,
-	faRightFromBracket,
-	faRobot,
-	faTrash,
-	faUsers,
-	faXmark, } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import router from "./router/Router";
 
-
 library.add(
-    fas,
-    faCheck,
-	faCirclePlus,
-	faEye,
-	faMessage,
-	faPaperPlane,
-	faPenToSquare,
-	faRightFromBracket,
-	faRobot,
-	faTrash,
-	faUsers,
-	faXmark
-    );
+  fas,
+  faCheck,
+  faCirclePlus,
+  faEye,
+  faMessage,
+  faPaperPlane,
+  faPenToSquare,
+  faRightFromBracket,
+  faRobot,
+  faTrash,
+  faUsers,
+  faXmark
+);
 
 const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
